a3c469f2e53c8885dfeb8f7261bd4915bad50366
Sebastian Hack <hack@cs.uni-saarland.de>
2023-12-12 05:47:10 +0100

    simplify

--- 2023/11/p11.py
+++ 2023/11/p11.py
@@ -4,44 +4,13 @@ from functools import reduce, cmp_to_key
 from itertools import combinations, permutations, product
 from collections import Counter
 
-def transpose(matrix):
-    return list(map(list, zip(*matrix)))
+# matrix transpose
+def transpose(m):
+    return list(map(list, zip(*m)))
 
-def twice(input):
-    res = []
-    for l in input:
-        if not '#' in l:
-            res += [l]
-        res += [l]
-
-    rest = transpose(res)
-    res = []
-    for l in rest:
-        if not '#' in l:
-            res += [l]
-        res += [l]
-    return transpose(res)
-
-abs = lambda x: x if x >= 0 else -x
-
-def p1(input):
-    uni = twice(input)
-    galaxies = list()
-    for y, l in enumerate(uni):
-        for x, c in enumerate(l):
-            if c == '#':
-                galaxies += [(x, y)]
-
-    res = 0
-    for (x0, y0), (x1, y1) in combinations(galaxies, 2):
-        d = abs(x1 - x0) + abs(y1 - y0)
-        res += d
-    print(res)
-
-def p2(input):
+def solve(input, size):
     rows = set(i for i, l in enumerate(input) if not '#' in l)
     cols = set(i for i, l in enumerate(transpose(input)) if not '#' in l)
-    size = 1000000
     galaxies = list()
     for y, l in enumerate(input):
         for x, c in enumerate(l):
@@ -58,5 +27,5 @@ def p2(input):
 
 input = [ l.strip() for l in open(0) ]
 
-p1(input)
-p2(input)
+solve(input, 2)
+solve(input, 1000000)
