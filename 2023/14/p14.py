e661da1e8ed0fa837cf62d014ff223a1839d58e9
Sebastian Hack <hack@cs.uni-saarland.de>
2023-12-14 18:57:38 +0100

    polishing and fixing

--- 2023/14/p14.py
+++ 2023/14/p14.py
@@ -1,89 +1,58 @@
-import re
-import math
-from functools import reduce, cmp_to_key
-from itertools import combinations, permutations, product
-from collections import Counter
+def transpose(input):
+    return [ list(l) for l in zip(*input) ]
+
+def tilt_west(input):
+    return [ '#'.join([ ''.join(sorted(l)[::-1]) for l in ''.join(s).split('#') ]) for s in input ]
 
 def tilt_north(input):
-    def find_pos(row, col):
-        r = row - 1
-        while r >= 0:
-            if input[r][col] == '.':
-                r -= 1
-            else:
-                return r + 1
-        return 0
-    for row, l in enumerate(input):
-        for col, c in enumerate(l):
-            if c == 'O':
-                r = find_pos(row, col)
-                if r < row:
-                    input[r][col] = 'O'
-                    input[row][col] = '.'
+    return transpose(tilt_west(transpose(input)))
+
+def tilt_south(input):
+    return list(reversed(tilt_north(reversed(input))))
+
+def tilt_east(input):
+    rev = lambda l: list(reversed(l))
+    return list(map(rev, tilt_west(map(rev, input))))
+
 
 def weight(input):
-    res = 0
-    for i, l in enumerate(input):
-        w = len(input) - i
-        n = sum(c == 'O' for c in l)
-        res += w * n
-    return res
+    return sum((len(input) - i) * \
+                sum(c == 'O' for c in l) for i, l in enumerate(input))
 
 def map_to_str(input):
     return '\n'.join([''.join(l) for l in input])
 
 def print_map(input):
-    print(map_to_str(input))
+    print('---\n', map_to_str(input))
 
 def p1(input):
-    tilt_north(input)
-    print(weight(input))
+    print(weight(tilt_north(input)))
 
 def cycle(input):
-    def transpose(input):
-        return [ list(l) for l in zip(*input) ]
-
-    # north
-    tilt_north(input)
-
-    # west
-    input = transpose(input)
-    tilt_north(input)
-    input = transpose(input)
-
-    # south
-    input.reverse()
-    tilt_north(input)
-    input.reverse()
-
-    # east
-    input = transpose(input)
-    input.reverse()
-    tilt_north(input)
-    input.reverse()
-    input = transpose(input)
-
+    input = tilt_north(input)
+    input = tilt_west(input)
+    input = tilt_south(input)
+    input = tilt_east(input)
     return input
 
 def p2(input):
     m = list(map(list, input))
     n = 1000000000
     seen = {}
-    for i in range(n):
+    for i in range(1, n):
+        input = cycle(input)
         s = map_to_str(input)
         if s in seen:
             start = seen[s]
-            repeat = i - start - 1
-            break
+            rest = (n - start) % (i - start)
+            for i in range(rest):
+                input = cycle(input)
+            print(weight(input))
+            return
         else:
             seen[s] = i
-        input = cycle(input)
-    rest = n % repeat
-    for i in range(start + repeat + rest):
-        m = cycle(m)
-    print(weight(m))
 
-input = [ [ c for c in l.strip() ] for l in open(0) ]
+input = [ list(l.strip()) for l in open(0) ]
 
 p1(input)
 p2(input)
