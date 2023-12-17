7a494c29568c3ff48d461dfde367d3f04415c3fa
Sebastian Hack <hack@cs.uni-saarland.de>
2023-12-17 11:14:05 +0100

    more polishing

--- 2023/17/p17.py
+++ 2023/17/p17.py
@@ -1,10 +1,13 @@
 from heapq import heappush, heappop
 
 def search(map, new_nn):
-    q = [ (0, 0, 0, 1, 0, 0) ]
-    seen = {}
+    q = [ (0, 0, 0, 1, 0, 0), (0, 0, 0, 0, 1, 0) ]
+    seen = set()
+    solutions = []
     while q:
         d, x, y, dx, dy, n = heappop(q)
+        if (x, y) == (X - 1, Y - 1):
+            solutions.append((n, d))
         for ndx, ndy in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
             if (ndx, ndy) == (-dx, -dy):
                 continue
@@ -12,32 +15,32 @@ def search(map, new_nn):
             ny = y + ndy
             if not (0 <= nx < X) or not (0 <= ny < Y):
                 continue
-            nn, skip = new_nn((x, y), (dx, dy), (ndx, ndy), n)
+            nn, skip = new_nn((dx, dy), (ndx, ndy), n)
             if skip:
                 continue
             nd = map[ny][nx] + d
             m = (nx, ny, ndx, ndy, nn)
-            if not m in seen or seen[m] > nd:
+            if not m in seen:
                 heappush(q, (nd,) + m)
-                seen[m] = nd
-    return seen
+                seen.add(m)
+    return solutions
 
 def p1(input):
-    def new_nn(pos, dir, new_dir, n):
+    def new_nn(dir, new_dir, n):
         nn = n + 1 if dir == new_dir else 1
         return (nn, nn > 3)
-    seen = search(input, new_nn)
-    print(min([ seen[m] for m in seen if m[:2] == (X - 1, Y - 1) ]))
+    solutions = search(input, new_nn)
+    print(min(d for n, d in solutions))
 
 def p2(input):
-    def new_nn(pos, dir, new_dir, n):
+    def new_nn(dir, new_dir, n):
         if dir == new_dir:
             nn = n + 1
             return (nn, nn > 10)
         else:
-            return (1, pos != (0, 0) and n < 4)
-    seen = search(input, new_nn)
-    print(min([ seen[m] for m in seen if m[:2] == (X - 1, Y - 1) and m[4] >= 4]))
+            return (1, n < 4)
+    solutions = search(input, new_nn)
+    print(min(d for n, d in solutions if n >= 4))
 
 input = [ [ int(i) for i in l.strip() ] for l in open(0) ]
 X = len(input[0])
