3da01fb04b6b5da521161949a5f17b564d0e4a0f
Sebastian Hack <hack@cs.uni-saarland.de>
2023-12-19 13:51:50 +0100

    made things smaller; oh my

--- 2023/19/d19p1.py
+++ 2023/19/d19p1.py
@@ -11,54 +11,30 @@ for wf in wfs.split('\n'):
     wf = wf[:-1]
     name, rest = wf.split('{')
     rs = []
-    for r in re.findall('(\w+)(.)(\d+)(:(\w+))?', rest):
-        rs += [ (r[0], r[1], int(r[2]), r[4]) ]
+    for r in re.findall('(\w+)(.)(\d+):(\w+)?', rest):
+        rs += [ (r[0], r[1], int(r[2]), r[3]) ]
     final = rest.split(',')[-1]
-    rules[name] = (rs, final)
+    rules[name] = rs + [("x", 't', 0, final)]
 
 cmp = {
     '<': lambda a, b : a < b,
     '>': lambda a, b : a > b,
+    't': lambda a, b : True,
 }
+
+def eval_wf(name, p):
+    for v, c, n, d in rules[name]:
+        if cmp[c](p[v], n):
+            match d:
+                case 'A':
+                    return sum(p.values())
+                case 'R':
+                    return 0
+                case _:
+                    return eval_wf(d, p)
+
 ratings = 0
 for p in pts.split('\n'):
     p = { r[0]: int(r[1]) for r in re.findall('(\w+)=(\d+)', p) }
-    curr = 'in'
-    rejected = False
-    accepted = False
-    print(p, sum(p.values()))
-    while not accepted and not rejected:
-        rs, final = rules[curr]
-        print(' ', curr, rs, final)
-        redirected = False
-        for r in rs:
-            if cmp[r[1]](p[r[0]], r[2]):
-                dest = r[3]
-                if dest == 'A':
-                    accepted = True
-                elif dest == 'R':
-                    rejected = True
-                else:
-                    redirected = True
-                    curr = dest
-                break
-        print('  ', redirected, accepted, rejected, curr)
-        if not redirected and not accepted and not rejected:
-            if final == 'A':
-                accepted = True
-            elif final == 'R':
-                rejected = True
-            else:
-                curr = final
-    if accepted:
-        print('  accept')
-        ratings += sum(p.values())
-
+    ratings += eval_wf('in', p)
 print(ratings)
-
-
-
-
-
-
-
