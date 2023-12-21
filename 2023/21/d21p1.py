b867216c7d586f5588afcc1944cd8d71e0b1bb25
Sebastian Hack <hack@cs.uni-saarland.de>
2023-12-21 18:30:18 +0100

    removed useless modules

--- 2023/21/d21p1.py
+++ 2023/21/d21p1.py
@@ -1,9 +1,3 @@
-import re
-import math
-from functools import reduce, cmp_to_key
-from itertools import combinations, permutations, product
-from collections import Counter, deque
-
 input = [ l.strip() for l in open(0) ]
 
 print(input)
